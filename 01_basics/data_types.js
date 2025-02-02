6edbe83524e487b9edc0605a9435ebd3e34e6f0d
krishna <dhekkrishna1@gmail.com>
2025-02-02 08:03:40 +0530

    JS Data Types

--- 01_basics/data_types.js
+++ 01_basics/data_types.js
@@ -49,3 +49,46 @@ console.log(typeof isWorking); //object, its to maintain the compatibility with 
 
 let check = Symbol('check');
 console.log(check, typeof check);//Symbol(check) symbol
+
+
+
+// Reference Type
+
+/**
+ * a. Array
+ * b. Objects
+ * c. Functions
+ */
+
+
+/**
+ * Array are reference type in js
+ * They are used to store hetrogeneous data
+ * Unlike other languages JS array are not guaranted to be stored in contiguous memory
+ * Js array are special objects, indexed numerically
+ */
+
+const arrayItems = [3, 23, 5, 6];
+console.log(arrayItems, typeof arrayItems); //[ 3, 23, 5, 6 ] object
+
+
+
+/**
+ * Objects are reference type
+ * They store data in key-value pairs
+ * Object are mutable i.e can be changed
+ * Unlike primitive data types, objects are stored in memory by reference.
+ */
+
+const data = {
+    name: 'John',
+    age: 30,
+    profession: 'Engineer'
+};
+
+console.log(data, typeof data); //{ name: 'Jhone', age: 30, profession: 'Engineer' } object
+
+//Updating the object using '.' notation
+data.age = 42;
+data.profession = 'Teacher';
+console.log(data); //{ name: 'Jhone', age: 42, profession: 'Teacher' }
