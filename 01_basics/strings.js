d17ab30eff1291e2a12c5b54d2848b7c77dcc129
krishna <dhekkrishna1@gmail.com>
2025-02-04 21:12:37 +0530

    String Template in JS

---
+++ 01_basics/strings.js
@@ -0,0 +1,8 @@
+let charString = "Hello";
+console.log(charString);
+
+//String Template
+let tempExample = 'This is 1st example';
+let tempExample1 = `Hey, ${tempExample}`;
+console.log(tempExample1);
+
