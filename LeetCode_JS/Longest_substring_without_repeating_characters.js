d1677ba1be831c8aec66f538ed38af0ad6c0059a
krishna <dhekkrishna1@gmail.com>
2025-02-06 17:03:29 +0530

    Solution to Leet Code Problems in JavaScript

---
+++ LeetCode_JS/Longest_substring_without_repeating_characters.js
@@ -0,0 +1,25 @@
+
+
+let lengthOfLongestSubstring = function(s) {
+  const set = new Set();
+  let i = 0;
+  let j = 0;
+  let len = 0;
+
+  while (j < s.length) {
+    if (!set.has(s.charAt(j))) {
+      set.add(s.charAt(j));
+      j++;
+      len = Math.max(len, set.size);
+    } else {
+      set.delete(s.charAt(i));
+      i++;
+    }
+  }
+  return len;
+};
+
+
+let str = "abcdabcbb";
+console.log("Length of Longest Substring is: "+lengthOfLongestSubstring(str));
+
