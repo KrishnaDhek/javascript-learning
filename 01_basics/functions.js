9cee2e110ecf62e6a7b9a7777d868eb1eed7ce1d
krishna <dhekkrishna1@gmail.com>
2025-02-08 21:23:24 +0530

    Arrow Function in JS

--- 01_basics/functions.js
+++ 01_basics/functions.js
@@ -1,4 +1,13 @@
 function funC(a, b) {
     return a + b;
 }
-console.log(funC(2400,450));
+console.log(funC(2400, 450));
+
+
+//arrow function
+
+let result = (a) => {
+    return a * a;
+}
+
+console.log(result(5));
