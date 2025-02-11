04726a027f0649479964907b3d966be3b8b27836
krishna <dhekkrishna1@gmail.com>
2025-02-11 17:15:32 +0530

    functions in JS

--- 01_basics/functions.js
+++ 01_basics/functions.js
@@ -1,36 +1,44 @@
-function funC(a, b) {
-    return a + b;
+// function funC(a, b) {
+//     return a + b;
+// }
+// console.log(funC(2400, 450));
+
+
+// //arrow function
+
+// let result = (a) => {
+//     return a * a;
+// }
+
+// console.log(result(5));
+
+// //Factorial
+
+// let fact = (n)=>{
+//     if (n === 0 || n === 1) {
+//         return 1;
+//     }
+//     else {
+//         return n * fact(n - 1);
+//     }
+// }
+
+// console.log("Factorial of the given number is: " + fact(5));
+
+
+// //Immediately Invoked Function Expression
+
+// let sqaure = (function (a) {
+//     return a * a;
+// })(12);
+
+// console.log(sqaure);
+
+
+
+function test() {
+    console.log(a);
+    var a = 20;
+    console.log(a);
 }
-console.log(funC(2400, 450));
-
-
-//arrow function
-
-let result = (a) => {
-    return a * a;
-}
-
-console.log(result(5));
-
-//Factorial
-
-let fact = (n)=>{
-    if (n === 0 || n === 1) {
-        return 1;
-    }
-    else {
-        return n * fact(n - 1);
-    }
-}
-
-console.log("Factorial of the given number is: " + fact(5));
-
-
-//Immediately Invoked Function Expression
-
-let sqaure = (function (a) {
-    return a * a;
-})(12);
-
-console.log(sqaure);
-
+test();
