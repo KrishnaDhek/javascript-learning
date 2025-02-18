30c5fa5de23fce1880a8b773487569510d123a59
krishna <dhekkrishna1@gmail.com>
2025-02-18 23:04:08 +0530

    Demonstration of Array Methods in JS

--- 01_basics/arrays.js
+++ 01_basics/arrays.js
@@ -1,74 +1,87 @@
-// // const items = ['mango', 'orange', 'apple'];
+// // // const items = ['mango', 'orange', 'apple'];
 
-// // items.forEach(function (item) {
-// //    console.log(item); 
+// // // items.forEach(function (item) {
+// // //    console.log(item); 
+// // // });
+
+
+// // const students = [
+// //   {
+// //     id: 1,
+// //     name: 'Ram',
+// //     class: 2,
+// //     age: 7,
+// //   },
+// //   {
+// //     id: 2,
+// //     name: 'Shyam',
+// //     class: 3,
+// //     age: 8,
+// //   },
+// //   {
+// //     id: 3,
+// //     name: 'Riya',
+// //     class: 4,
+// //     age: 10,
+// //   }
+// // ];
+
+// // console.log(typeof students);
+
+
+// // students.forEach(function (student) {
+// //     console.log(student.name);
 // // });
 
+// // const classArray = students.map(function (student) {
+// //     return student.name;
+// // });
+// // console.log(classArray)
 
-// const students = [
-//   {
-//     id: 1,
-//     name: 'Ram',
-//     class: 2,
-//     age: 7,
-//   },
-//   {
-//     id: 2,
-//     name: 'Shyam',
-//     class: 3,
-//     age: 8,
-//   },
-//   {
-//     id: 3,
-//     name: 'Riya',
-//     class: 4,
-//     age: 10,
-//   }
-// ];
+// const arr = ['krishna', 20, true,'time', 2025];
+// // console.log(arr[0]);
+//  console.log("length of array: "+arr.length);
 
-// console.log(typeof students);
+// // for (let i = 0; i < arr.length; i++){
+// //   console.log(arr[i]);
+// // }
+// let i=0
+// // while (i<arr.length) {
+// //   console.log(arr[i]);
+// //   i++;
+// // }
+
+// // arr.push('work');
+// // console.log('length of array: ' + arr.length);
+// // while (i < arr.length) {
+// //   console.log(arr[i]);
+// //   i++;
+// // }
 
 
-// students.forEach(function (student) {
-//     console.log(student.name);
-// });
+// arr.unshift(29);
+// console.log("length of array: " + arr.length);
 
-// const classArray = students.map(function (student) {
-//     return student.name;
-// });
-// console.log(classArray)
-
-const arr = ['krishna', 20, true,'time', 2025];
-// console.log(arr[0]);
- console.log("length of array: "+arr.length);
-
-// for (let i = 0; i < arr.length; i++){
-//   console.log(arr[i]);
-// }
-let i=0
-// while (i<arr.length) {
-//   console.log(arr[i]);
-//   i++;
-// }
-
-// arr.push('work');
-// console.log('length of array: ' + arr.length);
 // while (i < arr.length) {
 //   console.log(arr[i]);
 //   i++;
 // }
 
+// console.log("Removed item is: " + arr.shift());
+// console.log('length of array: ' + arr.length);
+// console.log('Removed item is: ' + arr.shift());
+// console.log('length of array: ' + arr.length);
 
-arr.unshift(29);
-console.log("length of array: " + arr.length);
 
-while (i < arr.length) {
-  console.log(arr[i]);
-  i++;
+
+const courses = ['Java', 'JS', 'C', 'C++', 'PHP'];
+
+for (let course of courses) {
+  console.log(course);
 }
 
-console.log("Removed item is: " + arr.shift());
-console.log('length of array: ' + arr.length);
-console.log('Removed item is: ' + arr.shift());
-console.log('length of array: ' + arr.length);
+
+
+//forEach method
+// courses.forEach(course => console.log(course));
 
