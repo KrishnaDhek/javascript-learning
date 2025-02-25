084e48872cda28e581fa9b2a0c1a810781349c54
krishna <dhekkrishna1@gmail.com>
2025-02-25 22:18:14 +0530

    Demonstration of Array Methods in JS

--- 01_basics/arrays.js
+++ 01_basics/arrays.js
@@ -1,154 +1,154 @@
 
-// // const items = ['mango', 'orange', 'apple'];
+ const items = ['mango', 'orange', 'apple'];
 
-// //  items.forEach(function (item) {
-// //       console.log(item); 
+  items.forEach(function (item) {
+       console.log(item); 
 
-// //   });
-
-
-// // const students = [
-// //   {
-// //     id: 1,
-// //     name: 'Ram',
-// //     class: 2,
-// //     age: 7,
-// //   },
-// //   {
-// //     id: 2,
-// //     name: 'Shyam',
-// //     class: 3,
-// //     age: 8,
-// //   },
-// //   {
-// //     id: 3,
-// //     name: 'Riya',
-// //     class: 4,
-// //     age: 10,
-// //   }
-// // ];
-
-//  console.log(typeof students);
-
-//  students.forEach(function (student) {
-//     console.log(student.name);
-// });
-
-// const classArray = students.map(function (student) {     return student.name;
-// }); console.log(classArray)
-
-// const newArr = ['tim', 'josh', 'john', 'lee'];
-// let portion = newArr.slice(1, 3);
-// console.log(portion);
-// console.log(newArr.some(check));
-// function check(element) {
-//   return element == 'krish';
-// }
+   });
 
 
-// const privilege = ['admin', 'member', 'super-admin'];
+ const students = [
+   {
+     id: 1,
+     name: 'Ram',
+     class: 2,
+     age: 7,
+   },
+   {
+     id: 2,
+     name: 'Shyam',
+     class: 3,
+     age: 8,
+   },
+   {
+     id: 3,
+     name: 'Riya',
+     class: 4,
+     age: 10,
+   }
+ ];
 
-// const privilageType = privilege.some(privilege => privilege === 'admin');
-// console.log(privilageType);
+ console.log(typeof students);
+
+ students.forEach(function (student) {
+    console.log(student.name);
+});
+
+const classArray = students.map(function (student) {     return student.name;
+}); console.log(classArray)
+
+const newArr = ['tim', 'josh', 'john', 'lee'];
+let portion = newArr.slice(1, 3);
+console.log(portion);
+console.log(newArr.some(check));
+function check(element) {
+  return element == 'krish';
+}
 
 
-// /
+const privilege = ['admin', 'member', 'super-admin'];
 
-// const arr = ['krishna', 20, true,'time', 2025];
-// // console.log(arr[0]);
-//  console.log("length of array: "+arr.length);
-
-// // for (let i = 0; i < arr.length; i++){
-// //   console.log(arr[i]);
-// // }
-// let i=0
-// // while (i<arr.length) {
-// //   console.log(arr[i]);
-// //   i++;
-// // }
-
-// arr.push('work');
-// console.log('length of array: ' + arr.length);
-// while (i < arr.length) {
-//   console.log(arr[i]);
-//   i++;
-// }
+const privilageType = privilege.some(privilege => privilege === 'admin');
+console.log(privilageType);
 
 
-// arr.unshift(29);
-// console.log("length of array: " + arr.length);
 
-// while (i < arr.length) {
-//   console.log(arr[i]);
-//   i++;
-// }
 
-// console.log("Removed item is: " + arr.shift());
-// console.log('length of array: ' + arr.length);
+const arr = ['krishna', 20, true,'time', 2025];
+ console.log(arr[0]);
+  console.log("length of array: "+arr.length);
+
+ for (let i = 0; i < arr.length; i++){
+   console.log(arr[i]);
+ }
+let i=0
+ while (i<arr.length) {
+   console.log(arr[i]);
+   i++;
+ }
+
+ arr.push('work');
+ console.log('length of array: ' + arr.length);
+ while (i < arr.length) {
+   console.log(arr[i]);
+   i++;
+ }
+
+
+ arr.unshift(29);
+ console.log("length of array: " + arr.length);
+
+ while (i < arr.length) {
+   console.log(arr[i]);
+   i++;
+ }
+
+ console.log("Removed item is: " + arr.shift());
+ console.log('length of array: ' + arr.length);
 // console.log('Removed item is: ' + arr.shift());
 // console.log('length of array: ' + arr.length);
 
 
 
-// const courses = ['Java', 'JS', 'C', 'C++', 'PHP'];
+ const courses = ['Java', 'JS', 'C', 'C++', 'PHP'];
 
-// for (let course of courses) {
-//   console.log(course);
-// }
+ for (let course of courses) {
+   console.log(course);
+ }
 
-// console.log("Contains the course true/false? " + courses.includes('Python')); //O(n)
-// console.log('Contains the course true/false? ' + courses.includes('JS'));
-
-
-// //forEach method
-// courses.forEach(course => console.log(course));
-
-// courses.splice(2, 0, 'Python');
-// console.log(courses);
-// console.log('Contains the course true/false? ' + courses.includes('Python'));
-
-// console.log(courses.unshift("Ruby"));//will return new length 
-// for(let item of courses){
-//   console.log(item);
-// }
-
-// console.log(courses.indexOf('js'));
-// console.log(courses);
-// console.log(delete courses[2]);
-// courses.splice(0,1)
-// console.log(courses);
-// console.log(courses);
-// let result = courses.slice(2);
-// console.log(result);
+console.log("Contains the course true/false? " + courses.includes('Python')); //O(n)
+console.log('Contains the course true/false? ' + courses.includes('JS'));
 
 
-// const elements = ['A', 'B', 'C', 'D', 'E', 'F'];
-// console.log(elements);
+//forEach method
+courses.forEach(course => console.log(course));
 
-// let newElements = elements.slice(1,3);
-// console.log(newElements);
+courses.splice(2, 0, 'Python');
+console.log(courses);
+console.log('Contains the course true/false? ' + courses.includes('Python'));
 
-// console.log(elements.reverse());
-// var items= newElements.join(",")
-// var result = items.split(",");
-// console.log(result);
+console.log(courses.unshift("Ruby"));//will return new length 
+for(let item of courses){
+  console.log(item);
+}
 
-// const nums = [2, 3, 4, 5];
-// var numSquare = nums.map(num => num **2);
-// console.log("Sqaure of each number present in the number array is: " + numSquare);
-
-// var numbSqaure = nums.map(funSquare);
-// function funSquare(num) {
-//   return num ** 2;
-// }
-
-// console.log(numbSqaure);
-
+console.log(courses.indexOf('js'));
+console.log(courses);
+console.log(delete courses[2]);
+courses.splice(0,1)
+console.log(courses);
+console.log(courses);
+let result = courses.slice(2);
+console.log(result);
 
 
-// const numbers = [1, 2, 3];
-// const squared = numbers.map(num => num ** 2);
-// console.log(squared); // [1, 4, 9]
+const elements = ['A', 'B', 'C', 'D', 'E', 'F'];
+console.log(elements);
+
+let newElements = elements.slice(1,3);
+console.log(newElements);
+
+console.log(elements.reverse());
+var items= newElements.join(",")
+var result = items.split(",");
+console.log(result);
+
+const nums = [2, 3, 4, 5];
+var numSquare = nums.map(num => num **2);
+console.log("Sqaure of each number present in the number array is: " + numSquare);
+
+var numbSqaure = nums.map(funSquare);
+function funSquare(num) {
+  return num ** 2;
+}
+
+console.log(numbSqaure);
+
+
+
+const numbers = [1, 2, 3];
+const squared = numbers.map(num => num ** 2);
+console.log(squared); // [1, 4, 9]
 
 
 const names = ["John", "Alice", "Bob"];
