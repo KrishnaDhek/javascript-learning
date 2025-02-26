430b3a5e7f4e8d2f9c3773005d95bb2907d09373
krishna <dhekkrishna1@gmail.com>
2025-02-27 00:09:16 +0530

    Achieve asynchronous behaviour of js using callback setTimeout function

--- 01_basics/callbacks.js
+++ 01_basics/callbacks.js
@@ -1,3 +1,5 @@
+//A callback function is a function that is passed as an argument to another function and executed later inside that function
+
 function first(callback) {
     console.log('this is first function');
     callback(third);
@@ -13,3 +15,18 @@ let third = function () {
 }
 
 first(second);
+
+
+//Callback function are a way to achieve the asynchronous behaviour of javascript
+
+function calculateTime(operation) {
+    setTimeout(() =>
+    { console.log(`operation took 3s seconds to complete`); }, 3000);
+    operation();
+}
+
+function operation() {
+    console.log('krishna');
+}
+
+calculateTime(operation);
