685e535ceb255785623592d871c71cce4a6278f8
krishna <dhekkrishna1@gmail.com>
2025-03-01 17:17:42 +0530

    practice functional programming using reduce  function

--- Practice-Ques/sumArray.js
+++ Practice-Ques/sumArray.js
@@ -1,4 +1,9 @@
 const nums = [4, 5, 6, 7, 8];
 
-const result = nums.reduce((acc, num) => acc + num, 0);
+const result = nums.reduce(function (acc, num) {
+    if (num > acc) {
+        acc = num;
+    }
+    return acc;
+},0);
 console.log(result);
