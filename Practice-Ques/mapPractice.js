d0a8f3a8ba9836ca55f6ae8e340fa8210069170d
krishna <dhekkrishna1@gmail.com>
2025-03-04 12:39:43 +0530

    Basic syntax of map()

---
+++ Practice-Ques/mapPractice.js
@@ -0,0 +1,6 @@
+//Write a function that returns a new array where each number is tripled.
+
+const numbers = [2, 4, 6, 8, 10];
+
+const newArray = numbers.map(num => num * 3);
+console.log(newArray);
