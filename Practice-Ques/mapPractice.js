e9ff91908bc7f898b46c809b1685986622ba398f
krishna <dhekkrishna1@gmail.com>
2025-03-04 12:51:51 +0530

    Map() to capitalize first letter

--- Practice-Ques/mapPractice.js
+++ Practice-Ques/mapPractice.js
@@ -4,3 +4,9 @@ const numbers = [2, 4, 6, 8, 10];
 
 const newArray = numbers.map(num => num * 3);
 console.log(newArray);
+
+
+//Write a function that returns a new array where the first letter of each name is capitalized.
+const names = ['alice', 'bob', 'charlie', 'dave'];
+const newArrayNames = names.map(name => name.charAt(0).toUpperCase()+name.slice(1).toLowerCase());
+console.log(newArrayNames);
