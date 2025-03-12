fe417a30389c93dd03976922bf7a7086620f21fd
krishna <dhekkrishna1@gmail.com>
2025-03-12 18:20:47 +0530

    High order function reduce demo

--- JS_Concepts/high_order_functions.js
+++ JS_Concepts/high_order_functions.js
@@ -1,6 +1,5 @@
 //Count occurrences of elements
 
-
 const words = ['apple', 'banana', 'apple', 'orange', 'banana', 'apple'];
 
 console.log(countFrequency(words));
@@ -22,30 +21,89 @@ console.log(countFrequency(words));
 //Count occurrences of elements using reduce method
 
 function countFrequency(words) {
-   return  words.reduce((acc, word) =>{
-        acc[word] = (acc[word] || 0) + 1;
-    return acc;},{}
- )
+  return words.reduce((acc, word) => {
+    acc[word] = (acc[word] || 0) + 1;
+    return acc;
+  }, {});
 }
-
 
 //reduce method practice questions
 //Multiply All Numbers
-const numbers = [2, 3, 4]; 
+const numbers = [2, 3, 4];
 const numMul = numbers.reduce(function (acc, num) {
-   return num * acc;
-},1)
+  return num * acc;
+}, 1);
 console.log(numMul);
 
-
-
 //Reverse an Array using
-const arr = [1, 2, 3, 4, 5]; 
+const arr = [1, 2, 3, 4, 5];
 
 const reversedArr = arr.reduce((acc, a) => {
-   // acc.unshift(a);
-   // return acc
+  // acc.unshift(a);
+  // return acc
 
-   return [a, ...acc]
-}, [])
+  return [a, ...acc];
+}, []);
 console.log(reversedArr);
+
+//Flatten a Nested Array
+const nestedArr = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+
+const newArr = nestedArr.reduce((acc, ar) => {
+  return acc.concat(ar);
+}, []);
+console.log(newArr);
+
+const nums = [1, 2, 3, 4, 5];
+
+const maxItem = nums.reduce((acc, n) => {
+  if (n > acc) acc = n;
+  return acc;
+}, 0);
+console.log(maxItem);
+
+const users = [
+  {
+    fName: 'krishna',
+    lName: 'dhek',
+    age: 28,
+  },
+
+  {
+    fName: 'vinita',
+    lName: 'dhek',
+    age: 28,
+  },
+
+  {
+    fName: 'mohit',
+    lName: '32',
+    age: 28,
+   },  
+  
+
+  {
+    fName: 'eva',
+    lName: 'mehra',
+    age: 4,
+  },
+];
+
+const userAge = users.reduce((acc, x) => {
+   
+   if (acc[x.age]) {
+      
+      acc[x.age]= ++acc[x.age];
+   }
+   else {
+      
+     acc[x.age] = 1;
+   }
+
+   return acc;
+}, {});
+console.log(userAge);
