43590593a2ecdbccaf2f171b5fa595f99fb3f884
krishna <dhekkrishna1@gmail.com>
2025-03-12 16:23:08 +0530

    High order function reduce demo

--- JS_Concepts/hight_order_functions.js
+++ JS_Concepts/hight_order_functions.js
@@ -5,18 +5,25 @@ const words = ['apple', 'banana', 'apple', 'orange', 'banana', 'apple'];
 
 console.log(countFrequency(words));
 
-function countFrequency(word) {
-    let map = new Map();
-    let count = 0;
-    for (w of word) {
-        if (map.has(w)) {
-            map.set(w, map.get(w)+1)
-        }
-        else {
-            map.set(w, 1);
-        }
-    }
-    return map;
-}
+// function countFrequency(word) {
+//     let map = new Map();
+//     let count = 0;
+//     for (w of word) {
+//         if (map.has(w)) {
+//             map.set(w, map.get(w)+1)
+//         }
+//         else {
+//             map.set(w, 1);
+//         }
+//     }
+//     return map;
+// }
 
 //Count occurrences of elements using reduce method
+
+function countFrequency(words) {
+   return  words.reduce((acc, word) =>{
+        acc[word] = (acc[word] || 0) + 1;
+    return acc;},{}
+ )
+}
