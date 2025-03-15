4118e2c9ccffc60c3cf583c32529314ddfcd9df0
krishna <dhekkrishna1@gmail.com>
2025-03-15 21:39:23 +0530

    Promise  example

---
+++ JS_Concepts/promises.js
@@ -0,0 +1,6 @@
+let stocks = {
+  Fruits: ['mango', 'apple', 'strawberry', 'pineapple', 'lyche', 'jackfruit'],
+  Flavour: ['chocolate', 'vanila', 'butterscotch', 'blueberry'],
+  Holder: ['cone', 'cup'],
+  Topping: ['sprinkle', 'raw-fruit'],
+};
