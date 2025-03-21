8f15e126d531db0d6027f55aabaaf385c06c6afd
krishna <dhekkrishna1@gmail.com>
2025-03-21 22:30:39 +0530

    Array Destructuring

--- JS_Concepts/arrays.js
+++ JS_Concepts/arrays.js
@@ -303,3 +303,12 @@ function reverseArray(arr) {
 console.log(reverseArray([1, 2, 3, 4, 5])); // [5, 4, 3, 2, 1]
 console.log(reverseArray([10, 20, 30]));    // [30, 20, 10]
 
+function reverseArray(arr) {
+  for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    [arr[i], arr[arr.length - 1 - i]] = [arr[arr.length - 1 - i], arr[i]]; 
+  }
+  return arr;
+}
+
+console.log(reverseArray([1, 2, 3, 4, 5])); // [5, 4, 3, 2, 1]
+console.log(reverseArray([10, 20, 30]));    // [30, 20, 10]
