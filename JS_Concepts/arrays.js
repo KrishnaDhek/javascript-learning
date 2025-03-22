1ad1779e2605c7309a431e5d53367747d116acbc
krishna <dhekkrishna1@gmail.com>
2025-03-22 16:38:57 +0530

    Array Destructuring

--- JS_Concepts/arrays.js
+++ JS_Concepts/arrays.js
@@ -327,8 +327,8 @@ const arr = [10, 20, 30, 40, 50];
 console.log(extractVal(arr));
 
 
-//Swap the first and last elements of an array using array destructuring.
 
+//Swap the first and last elements of an array using array destructuring.
 function funSwap(arr) {
   [arr[0], arr[arr.length-1]] = [arr[arr.length-1],arr[0]];
   return arr;
