0e3086e91de3ee3eb83541557025814cb6268452
krishna <dhekkrishna1@gmail.com>
2025-04-04 23:26:47 +0530

    Add script.js file

--- JS_With_Project/weather_app/script/script.js
+++ JS_With_Project/weather_app/script/script.js
@@ -1,2 +1,11 @@
 const apiKey = "4210ed527e25a0e7ae99fe4a5ad35cef";
-const apiURL = "";
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=london';
+
+async function checkWeather() {
+    const response = await fetch(apiURL + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+}
+
+checkWeather();
