efe55843053c1e42a1ad120ac0c8bca71413daf0
krishna <dhekkrishna1@gmail.com>
2025-04-06 23:47:57 +0530

    Revision of Objects

---
+++ Revision/Objects.js
@@ -0,0 +1,9 @@
+const person1 = {
+    'firstName': 'Krishna',
+    'lastName': 'Dhek',
+    'age': 29
+}
+console.log(person1);
+console.log(person1.age);
+console.log(person1[name]);
+console.log(person1.name);
