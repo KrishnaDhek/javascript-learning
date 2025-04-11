3118ebe835cb25a0105c0fb062d7a54e0e73e105
krishna <dhekkrishna1@gmail.com>
2025-04-12 00:40:17 +0530

    Add polyfill for filter()

--- JS_Concepts/polyfills.js
+++ JS_Concepts/polyfills.js
@@ -13,3 +13,22 @@ const result = arr.testingMap((a, i, arr) => {
     return a * 5;
 })
 console.log(result);
+
+
+//filter
+
+Array.prototype.myfilter = function (callBack) {
+    const newArr = [];
+    for (let i = 0; i < this.length; i++){
+        if (callBack(this[i], i, this)) {
+            newArr.push(this[i])
+        }
+    }
+    return newArr;
+}
+
+const arr = [33, 12, 44, 55, 645454, 11];
+const result = arr.myfilter((a, i, arr) => {
+    return a % 11 == 0;
+})
+console.log(result);
