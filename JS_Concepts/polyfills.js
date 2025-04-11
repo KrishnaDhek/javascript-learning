4d13389b99b8783cc54e1357a0c570b804d7fcca
krishna <dhekkrishna1@gmail.com>
2025-04-12 00:54:17 +0530

    Add polyfill for reduce()

--- JS_Concepts/polyfills.js
+++ JS_Concepts/polyfills.js
@@ -32,3 +32,20 @@ const result = arr.myfilter((a, i, arr) => {
     return a % 11 == 0;
 })
 console.log(result);
+
+
+//Reduce
+Array.prototype.myReduce = function (callBack,currVal) {
+    let acc = currVal !== undefined ? currVal : this[0];
+    let index = currVal !== undefined ? 0 : 1;
+    for (let i = index; i < this.length; i++){
+        acc = callBack(acc,this[i], i, this);
+    }
+    return acc;
+}
+
+const arr = [33, 12, 44, 55, 645454, 11];
+const result = arr.myReduce((acc, currVal) => {
+    return acc+currVal;
+});
+console.log(result);
