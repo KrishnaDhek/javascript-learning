b5956883c94c07d7f34611d8be8710a9d661a770
krishna <dhekkrishna1@gmail.com>
2025-04-12 00:00:01 +0530

    Practice prototype in JS

--- JS_Concepts/prototype.js
+++ JS_Concepts/prototype.js
@@ -160,3 +160,4 @@ const person1 = new Person("Tim");
 // console.log(Object.getOwnPropertyNames(person));
 console.log(Object.getOwnPropertyNames(person1.__proto__));
 console.log(Object.getOwnPropertyNames(person1));
+
