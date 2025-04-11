6fb59f34a96d14bfbfe1a796d0ef976f4056c405
krishna <dhekkrishna1@gmail.com>
2025-04-11 20:23:09 +0530

    Prototype in JS

---
+++ JS_Concepts/prototype.js
@@ -0,0 +1,15 @@
+/* Prototype in JavaScript:
+
+    In JavaScript, every object has a hidden property called [[Prototype]], which can be accessed using __proto__. This __proto__ creates the actual link used by the JavaScript engine to perform prototype chaining.
+    If a property or method is not found on the current object, JavaScript looks up the prototype chain — following __proto__ — until it either finds the property or reaches null, which marks the end of the chain.
+*/
+
+
+//Write a constructor function called Person that takes a name as a parameter and stores it as a property on the object.
+
+function Person(name) {
+    this.name = name;
+}
+
+const p = new Person("Krishna");
+console.log(p.name); // should print "Krishna"
