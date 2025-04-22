40325c433b4a5f3e8c51851632258412e7e19cb6
krishna <dhekkrishna1@gmail.com>
2025-04-22 16:48:14 +0530

    Map

--- Revision/Objects.js
+++ Revision/Objects.js
@@ -63,3 +63,85 @@ let obj = { a: 1 };
 delete obj.a;
 delete obj.b;
 console.log(obj);
+
+
+//Object Key Overwrite Check
+const obj1 = {};
+let key1 = {};
+let key2 = {};
+
+obj1[key1] = "value 1";
+obj1[key2] = "value 2";
+console.log(obj1); // js automatically converts it to string ["object Object"]
+console.log(obj1[key1]);// both the keys are now ["object Object"] thus values is overriden
+console.log(obj1[key2]);
+
+
+const map = new Map();
+map.set(key1, "value 1");
+map.set(key2, "value 2");
+
+console.log(map.get(key1));
+console.log(map.get(key2));
+console.log(map);
+
+
+
+//Storing and Accessing Mixed Types in a Map
+const person1 = { name: "Alice" };
+const person2 = { name: "Bob" };
+const obj = { name: "Charlie" };
+
+const map = new Map();
+
+map.set(person1, "Person 1's Info");
+map.set(person2, "Person 2's Info");
+map.set(obj, "Person 3's Info");
+
+console.log(map.get(person1)); //  Person1's info
+console.log(map.get(person2)); //  Person2's info
+console.log(map.get(obj)); // Person3's info 
+console.log(map);/**Map(3) {
+  { name: 'Alice' } => "Person 1's Info",
+  { name: 'Bob' } => "Person 2's Info",
+  { name: 'Charlie' } => "Person 3's Info"
+}
+ */
+
+//Object Manipulation
+const book = {
+    title: "JS Concept",
+    author: "ChatGPT",
+    price: 1000
+};
+book.price = 20;
+console.log(book);
+
+console.log("author" in book);
+console.log(book.hasOwnProperty("author"));
+for (let key in book) {
+    console.log(`${key} : ${book[key]}`);
+}
+
+//Section 2: Maps
+//Create an empty Map and add a key-value pair where the key is "name" and the value is "John". Log the Map.
+const map = new Map();
+map.set("name", "John");
+console.log(map);
+//Create a Map where the key is a number and the value is a string. Retrieve the value using the number as the key.
+map.set(123, "abc");
+console.log(map);
+//Create a Map with an object as a key (like { id: 1 }) and a string as a value. Log the Map and retrieve the value by using the object key.
+let key = { id: 1 };
+map.set(key, "testing");
+console.log(map);
+console.log(map.get(key));
+//Add a new entry to the Map with another object key ({ id: 2 }) and log the entire Map.
+let key1 = { id: 2 };
+map.set(key, 'check');
+console.log(map);
+console.log(map.get(key));
+
+
+
+//
