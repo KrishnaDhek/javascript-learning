3c586e73245a6677abdef836ec43f429ec1c61d0
krishna <dhekkrishna1@gmail.com>
2025-04-23 14:35:33 +0530

    Polyfill for apply()

--- Revision/applyPolyfill.js
+++ Revision/applyPolyfill.js
@@ -1,16 +1,24 @@
-//Polyfill for apply
+//Polyfill for apply() - takes two arguments current context and array or undefined 
 
 Function.prototype.myApply = function (currentContext, arg) {
-    if (typeof this !== 'function') {
-        throw new Error(`${this} must be a function`);
-    }
+  if (typeof this !== 'function') {
+    //check if the current content is a  function
+    throw new Error(`${this} must be a function`);
+  }
 
-    if (arg !== undefined && !Array.isArray(arg)) {
-        throw new TypeError(`Second argument must be array or undefined `)
-    }
-    currentContext.funC = this;
-    currentContext.funC(...arg || []);
-    delete currentContext.funC;
+  if (arg !== null && !Array.isArray(arg)) {
+    // check if second arg is either an array or undefined
+    throw new TypeError(`Second argument must be array or undefined `);
+  }
+
+  // Temporarily assign the function to the context
+  currentContext.funC = this;
+
+  // Call the function using spread syntax. If arg is undefined, fall back to an empty array
+  currentContext.funC(...(arg || []));
+
+  // Clean up the temporary property
+  delete currentContext.funC;
 }
 
 const obj1 = {
@@ -30,5 +38,5 @@ function getDetails(age, place) {
 
 getDetails.myApply(obj1, [28, "lohaghat"]);
 getDetails.myApply(obj2, [58, 'bangalore']);
-getDetails.myApply(obj3, 5);
+getDetails.myApply(obj3, null);
 getDetails.myApply(obj2);
