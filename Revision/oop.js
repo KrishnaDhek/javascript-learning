0e1d2319abdc78a75522ee8016afbf2708d297a9
krishna <dhekkrishna1@gmail.com>
2025-04-28 15:03:46 +0530

    OOP in JS

--- Revision/oop.js
+++ Revision/oop.js
@@ -4,6 +4,10 @@ class Animal{
         this.type = type;
         this.name = name;
     }
+
+    sound(sound) {
+        return `${this.name} makes the sound ${sound} `;
+    }
 }
 
 const dog = new Animal("dog", "J");
@@ -12,3 +16,5 @@ console.log(dog);
 console.log(cat);
 dog.name = 'Pete';
 console.log(dog);
+console.log(dog.sound('bark'));
+console.log(cat.sound('meow'));
