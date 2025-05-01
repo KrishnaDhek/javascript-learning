c4771c60b0268a358f3ee13101a6f70cd11e5aa3
krishna <dhekkrishna1@gmail.com>
2025-05-01 17:50:57 +0530

    File Explorer using React

--- Machine_Coding/file_explorer/src/App.js
+++ Machine_Coding/file_explorer/src/App.js
@@ -1,17 +1,69 @@
-import React from 'react';
-import FileExplorer from './components/FileExplorer';
-import data from './data/data.json';
+// import React, { useState } from "react";
+// import fileData from "./data/data.json"
+// import style from "./App.css"
+
+// // Render list of object
+// const List = ({ list }) => {
+//   const[isExpanded, setIsExpanded] =useState({})
+//   return (
+//     <div className="container">
+//       {list.map((node) => (
+//         <div key={node.id}>
+//           {node.isFolder && (
+//             <span onClick={() => setIsExpanded((prev) => ({ ...prev, [node.id]: !prev[node.id], }))
+//             }
+//             >
+//               {isExpanded?.[node.id]?"-":"+"}
+//             </span>
+//           )}
+//           <span>{node?.childNode ? "🗂️" : " 📄"}{node.name}</span>
+//           {isExpanded?.[node.id] && node?.childNode && <List list={node.childNode} />}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   const [data, setData] = useState(fileData);
+//   return (
+//     <div className="App">
+//       <h1>File Explorer</h1> 
+//       <List list={data} />
+//     </div>
+//   )
+// }
 
 
-function App() {
-  console.log(data); // Check if data is correctly imported
+import React, { useState } from 'react';
+import fileData from "./data/data.json"
+import style from './App.css';
 
+
+const List = ({ list }) => {
+  //Expand & Collaps
+  const[isExpanded, setIsExpanded] = useState({})
   return (
-    <div>
-      <h1>File Explorer</h1>
-      <FileExplorer data={data} />
+    <div className='container'>
+      {list.map((node) => (
+        <div key={node.id}>
+          {node.isFolder && (<span onClick={() => setIsExpanded((prev) => ({ ...prev, [node.id]: !prev[node.id] }))}>
+            {isExpanded?.[node.id]?"-":"+"}
+          </span>)}
+          <span>{node.childNode ? "🗂️" : "📄"}{node.name}</span>
+          {isExpanded?.[node.id] && node?.childNode && (<List list={node.childNode}/>)}
+        </div>
+      ))}
     </div>
-  );
+  )
 }
-
-export default App;
+export default function App() {
+  const [data, setData] = useState(fileData);
+  
+  return (
+    <div className='App'>
+      <h1>File Explorer</h1>
+      <List list ={data}/>
+   </div>
+  )
+}
