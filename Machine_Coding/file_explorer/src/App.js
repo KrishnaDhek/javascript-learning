e33c66f6c88c94fa436fceb7d3fcacabc1614289
krishna <dhekkrishna1@gmail.com>
2025-05-02 13:17:44 +0530

    File Explorer, expand & collaps feature

--- Machine_Coding/file_explorer/src/App.js
+++ Machine_Coding/file_explorer/src/App.js
@@ -1,57 +1,21 @@
-// import React, { useState } from "react";
-// import fileData from "./data/data.json"
-// import style from "./App.css"
-
-// // Render list of object
-// const List = ({ list }) => {
-//   const[isExpanded, setIsExpanded] =useState({})
-//   return (
-//     <div className="container">
-//       {list.map((node) => (
-//         <div key={node.id}>
-//           {node.isFolder && (
-//             <span onClick={() => setIsExpanded((prev) => ({ ...prev, [node.id]: !prev[node.id], }))
-//             }
-//             >
-//               {isExpanded?.[node.id]?"-":"+"}
-//             </span>
-//           )}
-//           <span>{node?.childNode ? "🗂️" : " 📄"}{node.name}</span>
-//           {isExpanded?.[node.id] && node?.childNode && <List list={node.childNode} />}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default function App() {
-//   const [data, setData] = useState(fileData);
-//   return (
-//     <div className="App">
-//       <h1>File Explorer</h1> 
-//       <List list={data} />
-//     </div>
-//   )
-// }
-
-
-import React, { useState } from 'react';
-import fileData from "./data/data.json"
-import style from './App.css';
+import React, { useState } from "react";
+import fileData from "./data/data.json";
+import "./App.css";
 
 
 const List = ({ list }) => {
-  //Expand & Collaps
-  const[isExpanded, setIsExpanded] = useState({})
+  const [isExpanded, setIsExpanded] = useState({});
   return (
-    <div className='container'>
+    <div className="container">
       {list.map((node) => (
         <div key={node.id}>
-          {node.isFolder && (<span onClick={() => setIsExpanded((prev) => ({ ...prev, [node.id]: !prev[node.id] }))}>
+          {node.isFolder && (<span onClick={() =>
+            setIsExpanded((prev) =>({...prev, [node.id]:!prev[node.id]}))
+          }>
             {isExpanded?.[node.id]?"-":"+"}
           </span>)}
-          <span>{node.childNode ? "🗂️" : "📄"}{node.name}</span>
-          {isExpanded?.[node.id] && node?.childNode && (<List list={node.childNode}/>)}
+          <span> {node.childNode ? "🗂️" : "📄"}{node.name}</span>
+          {isExpanded?.[node.id] &&node?.childNode && (<List list={node.childNode}/>)}
         </div>
       ))}
     </div>
@@ -59,11 +23,10 @@ const List = ({ list }) => {
 }
 export default function App() {
   const [data, setData] = useState(fileData);
-  
   return (
-    <div className='App'>
+    <div className="App">
       <h1>File Explorer</h1>
-      <List list ={data}/>
-   </div>
+      <List list={data}/>
+    </div>
   )
 }
