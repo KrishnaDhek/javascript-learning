c092d81838e9fc5dd35b47e1f044aaf20f5e076e
krishna <dhekkrishna1@gmail.com>
2025-05-03 15:09:09 +0530

    Promise practice questions

--- JS_Concepts/promises.js
+++ JS_Concepts/promises.js
@@ -1021,3 +1021,19 @@ stepByStep()
     console.log(msg);
   })
    
+
+function login(username, password) {
+  return new Promise((resolve, reject) => {
+    if (username === "admin" && password === "1234") {
+        resolve("Login Successful!")
+    } else {
+      setTimeout(() => {
+        reject('Invalid Credentials');
+      }, 1500);
+      }
+    })
+}
+  
+login("person", "1234")
+  .then((msg) => console.log(msg))
+.catch((err)=>console.log(err))
