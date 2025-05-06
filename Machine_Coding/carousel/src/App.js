ef3aa5809211f949787266546b04b954d2062944
krishna <dhekkrishna1@gmail.com>
2025-05-06 21:29:40 +0530

    Add Carousel Logic

--- Machine_Coding/carousel/src/App.js
+++ Machine_Coding/carousel/src/App.js
@@ -2,55 +2,51 @@ import React, { useEffect, useRef, useState } from "react"
 import data from "./data/imageData.json"
 import "./App.css"
 
-
-const ImageCarousel = () => {
+const Carousel = () => {
   const [index, setIndex] = useState(0);
-  const dataLength = data.length;
+  let dataLength = data.length;
   const ref = useRef(null);
 
-  const handlePrev =() => {
+  const handlePrev = () => {
     setIndex((prev) => {
       if (prev == 0) {
-        return dataLength-1;
+        return dataLength - 1;
+      } else {
+        return prev- 1; 
       }
-      else {
-        return prev - 1;
-      }
-  })
-    
+    })
   }
+
   const handleNext = () => {
     setIndex((prev) => {
       if (prev == dataLength - 1) {
         return 0;
       } else {
         return prev + 1;
-     }
-   })
-    
+      }
+    })
   }
 
   useEffect(() => {
     ref.current = setInterval(handleNext, 1000);
-    return ()=>clearInterval(ref.current)
+    return () => clearInterval(ref.current);
   },[])
 
   return (
-    <div onMouseEnter={() => clearInterval(ref.current)}
-      onMouseLeave={()=>{ref.current = setInterval(handleNext, 1000)}}
-      className="container">
-      <div onClick={handlePrev} className="leftBtn">{'<'}</div>
-      <img src={data[index].download_url} alt="imge"></img>
-      <div onClick={handleNext} className="rightBtn">{'>'}</div>
+    <div onMouseEnter={()=> clearInterval(ref.current)}
+      onMouseLeave={() => { ref.current = setInterval(handleNext, 1000) }} className="container">
+      <div onClick={handlePrev} className="leftBtn">{"<"}</div>
+      <img src={data[index].download_url}></img>
+      <div onClick={handleNext} className="rightBtn">{ ">"}</div>
     </div>
-  );
+  )
 }
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Image Carousel</h1>
-
-      <ImageCarousel/>
+      <h1>Carousel Component</h1>
+      <Carousel />
     </div>
   )
 }
