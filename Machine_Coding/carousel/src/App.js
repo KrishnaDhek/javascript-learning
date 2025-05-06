7b8c2972ff43186499e2d638fa136f8724ac53bc
krishna <dhekkrishna1@gmail.com>
2025-05-06 18:21:36 +0530

    Carousel in React

---
+++ Machine_Coding/carousel/src/App.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef, useState } from "react"
+import data from "./data/imageData.json"
+import "./App.css"
+
+
+const ImageCarousel = () => {
+  const [index, setIndex] = useState(0);
+  const dataLength = data.length;
+  const ref = useRef(null);
+
+  const handlePrev =() => {
+    setIndex((prev) => {
+      if (prev == 0) {
+        return dataLength-1;
+      }
+      else {
+        return prev - 1;
+      }
+  })
+    
+  }
+  const handleNext = () => {
+    setIndex((prev) => {
+      if (prev == dataLength - 1) {
+        return 0;
+      } else {
+        return prev + 1;
+     }
+   })
+    
+  }
+
+  useEffect(() => {
+    ref.current = setInterval(handleNext, 1000);
+    return ()=>clearInterval(ref.current)
+  },[])
+
+  return (
+    <div onMouseEnter={() => clearInterval(ref.current)}
+      onMouseLeave={()=>{ref.current = setInterval(handleNext, 1000)}}
+      className="container">
+      <div onClick={handlePrev} className="leftBtn">{'<'}</div>
+      <img src={data[index].download_url} alt="imge"></img>
+      <div onClick={handleNext} className="rightBtn">{'>'}</div>
+    </div>
+  );
+}
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Image Carousel</h1>
+
+      <ImageCarousel/>
+    </div>
+  )
+}
