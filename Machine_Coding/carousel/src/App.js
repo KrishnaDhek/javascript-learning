e58a6f8853f590c3268dd6bb82b497943af1fdc1
krishna <dhekkrishna1@gmail.com>
2025-05-07 19:13:58 +0530

    Carousel in React

--- Machine_Coding/carousel/src/App.js
+++ Machine_Coding/carousel/src/App.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useRef, useState } from "react"
+import React, { useState,useRef, useEffect} from "react"
 import data from "./data/imageData.json"
 import "./App.css"
 
 const Carousel = () => {
   const [index, setIndex] = useState(0);
-  let dataLength = data.length;
+  const dataLength = data.length;
   const ref = useRef(null);
 
   const handlePrev = () => {
@@ -12,7 +12,7 @@ const Carousel = () => {
       if (prev == 0) {
         return dataLength - 1;
       } else {
-        return prev- 1; 
+        return prev - 1;
       }
     })
   }
@@ -29,24 +29,42 @@ const Carousel = () => {
 
   useEffect(() => {
     ref.current = setInterval(handleNext, 1000);
-    return () => clearInterval(ref.current);
+    return ()=>clearInterval(ref.current)
   },[])
 
   return (
-    <div onMouseEnter={()=> clearInterval(ref.current)}
-      onMouseLeave={() => { ref.current = setInterval(handleNext, 1000) }} className="container">
-      <div onClick={handlePrev} className="leftBtn">{"<"}</div>
+    <div
+      onMouseEnter={() => clearInterval(ref.current)}
+      onMouseLeave={()=>{ref.current = setInterval(handleNext, 1000)}}
+      className="container">
+      <div onClick={handlePrev} className="leftBtn">
+        {'<'}
+      </div>
       <img src={data[index].download_url}></img>
-      <div onClick={handleNext} className="rightBtn">{ ">"}</div>
+      <div onClick={handleNext} className="rightBtn">
+        {'>'}
+      </div>
+      <div className="dotContainer">
+        {data.map((_, dotIndex) => {
+          return (
+            <span
+              key={dotIndex}
+              className={`dot ${index === dotIndex ? 'active' : ''}`}
+              onClick={() => setIndex(dotIndex)}
+            >
+              &#x2022;
+            </span>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
-
 export default function App() {
   return (
     <div className="App">
-      <h1>Carousel Component</h1>
-      <Carousel />
+      <h1>Carousel</h1>
+      <Carousel/>
     </div>
   )
 }
