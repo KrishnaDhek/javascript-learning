d8a152129188333b49beec132df6eb466a60fff6
krishna <dhekkrishna1@gmail.com>
2025-05-10 18:31:41 +0530

    Search bar using debouncing to limit api call

--- Machine_Coding/search_bar/src/App.js
+++ Machine_Coding/search_bar/src/App.js
@@ -1,43 +1,41 @@
-import React, { useCallback, useState } from "react";
-import "./App.css";
-import { FaSearch } from "react-icons/fa";
+import React, { useCallback, useState } from "react"
+import "./App.css"
+
+
 
 export default function App() {
   const [search, setSearch] = useState([]);
+
   const debounce = (func) => {
-    let timer;
-    return function (...args) {
+    let time;
+    return function(...args){
       const context = this;
-      if (timer) clearTimeout(timer);
-      timer=setTimeout(() => {
-        timer = null;
-        func.apply(context, args);
+      if (time) clearTimeout(time);
+      time = setTimeout(() => {
+        time = null;
+        func.apply(context,args)
       }, 500);
     }
   }
 
   const handleChange = (e) => {
-    const { value } = e.target;
-    fetch(`https://demo.dataverse.org/api/search?q=${value}`).then((response)=>response.json()).then(json =>setSearch(json.data.items))
+    const value = e.target.value;
+    fetch(`https://demo.dataverse.org/api/search?q=${value}`)
+      .then((response) => response.json())
+    .then((json)=>setSearch(json.data.items))
   }
-
-  const optVersion = useCallback(debounce(handleChange),[]);
+const betterHandle = useCallback(debounce(handleChange),[])
   return (
     <div className="App">
-      <h1>SearchBar</h1>
       <div className="searchContainer">
-        <div className="inputContainer">
-          <FaSearch className="searchIcon" />
-          <input type="text" placeholder="Search..." onChange={optVersion}/>
-        </div>
-        {search?.length > 0 && (
-          <div className="autocomplete">
-            {search.map((item, index) => (
-              <span key={index}>{ item.name}</span>
-            ))}
-          </div>
-        )}
+        <input type="text" placeholder="Search..."  onChange={betterHandle}/>
+        {search?.length > 0 &&
+          (<div className="autocomplete">
+          {search.map((item, index) => (
+            <span key={index}>{item.name}</span>
+          ))}
+        </div>)}
       </div>
-</div>    
+    </div>
   )
 }
