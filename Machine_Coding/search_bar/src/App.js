27590eed753fa21137d89905b4a7904c4674b42b
krishna <dhekkrishna1@gmail.com>
2025-05-10 21:49:35 +0530

    Search bar using debouncing to limit api call

--- Machine_Coding/search_bar/src/App.js
+++ Machine_Coding/search_bar/src/App.js
@@ -1,39 +1,38 @@
 import React, { useCallback, useState } from "react"
-import "./App.css"
-
-
+import "./App.css";
 
 export default function App() {
   const [search, setSearch] = useState([]);
-
   const debounce = (func) => {
-    let time;
-    return function(...args){
-      const context = this;
-      if (time) clearTimeout(time);
-      time = setTimeout(() => {
-        time = null;
-        func.apply(context,args)
+    let timer;
+    return function (...args) {
+      let context = this;
+      if (timer) clearTimeout(timer);
+
+      timer=setTimeout(() => {
+         timer = null;
+         func.apply(context, args);
       }, 500);
+     
+
     }
   }
 
   const handleChange = (e) => {
     const value = e.target.value;
+
     fetch(`https://demo.dataverse.org/api/search?q=${value}`)
       .then((response) => response.json())
-    .then((json)=>setSearch(json.data.items))
+      .then((json) => setSearch(json.data.items));
   }
-const betterHandle = useCallback(debounce(handleChange),[])
+
+  const betterHandle = useCallback(debounce(handleChange),[]);
   return (
     <div className="App">
       <div className="searchContainer">
-        <input type="text" placeholder="Search..."  onChange={betterHandle}/>
-        {search?.length > 0 &&
-          (<div className="autocomplete">
-          {search.map((item, index) => (
-            <span key={index}>{item.name}</span>
-          ))}
+        <input type="text" placeholder="Search..." onChange={betterHandle} />
+        {search?.length > 0 && (<div className="autocomplete">
+          {search.map((item, index) => (<span key={index}>{item.name}</span>))}
         </div>)}
       </div>
     </div>
