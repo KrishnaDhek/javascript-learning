262570a08ad9f7f90f2c80530b32c1f106807cc5
krishna <dhekkrishna1@gmail.com>
2025-05-11 18:05:24 +0530

    Pagination in React

--- Machine_Coding/pagination/src/App.js
+++ Machine_Coding/pagination/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+
+const Pagination = ({ products }) => {
+  const [page, setPage] = useState(1);
+
+  const selectHandle = (selectedPage) => {
+    if(selectedPage>=1 && selectedPage<=products.length/10 && selectedPage !==page)
+    setPage(selectedPage);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="productsContainer">
+      {products.length > 0 && (
+        <div className="products">
+          {products.slice(page * 10 - 10, page * 10).map((item, index) => (
+            <span key={index} className="productsSingle">
+              <img src={item.thumbnail} alt={item.title} />
+              <span>{item.title}</span>
+            </span>
+          ))}
+        </div>
+      )}
+      {products.length > 0 && (
+        <div className="pagination">
+          <span
+            onClick={() => selectHandle(page - 1)}
+            className={page >1 ? '' : 'paginationDisabled'}
+          >
+            ⬅️
+          </span>
+          {[...Array(products.length / 10)].map((_, index) => {
+            return (
+              <span
+                className={page === index + 1 ? 'paginationSelected' : ''}
+                onClick={() => selectHandle(index + 1)}
+                key={index}
+              >
+                {index + 1}
+              </span>
+            );
+          })}
+
+          <span
+            onClick={() => selectHandle(page + 1)}
+            className={page < products.length / 10 ? '' : 'paginationDisabled'}
+          >
+            ➡️
+          </span>
+        </div>
+      )}
     </div>
   );
 }
+export default function App() {
+  const[products,setProducts]=useState([]);
 
-export default App;
+  const fetchData = async () => {
+    const response = await fetch(`https://dummyjson.com/products?limit=100`);
+    const data = await response.json();
+
+    if(data && data.products) {
+    setProducts(data.products);
+   }
+  };
+  console.log(products);
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+  
+
+  
+  
+  return (
+    <div className="App">
+      <h1>Pagination Component</h1>
+      <Pagination products={products} />
+    </div>
+  )
+}
