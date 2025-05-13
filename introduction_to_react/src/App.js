8536fb40440000e563859fc49e8a63efcc321092
krishna <dhekkrishna1@gmail.com>
2025-05-13 16:15:42 +0530

    Introduction to event, usestate

--- introduction_to_react/src/App.js
+++ introduction_to_react/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
 
-function App() {
+export default function App() {
+
+  const [input, setInput] = useState("");
+  const [userName, setUserName] = useState([]);
+  const [debouncedInput, setDebouncedInput] = useState("");
+
+  useEffect(() => {
+    const timer=setTimeout(() => {
+      setDebouncedInput(input);
+       console.log('Debounced input set:', input); 
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+      console.log('Typing... debounce reset');
+    }
+  }, [input])
+  
+  const handleChange = (e) => {
+   
+    setInput(e.target.value)
+    
+  }
+
+  const btnClick = () => {
+    console.log("Button Clicked");
+    if (debouncedInput.trim() !== "") {
+      setUserName([...userName, debouncedInput]);
+      setInput("");
+      setDebouncedInput("");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Introduction to React</h1>
+      <input
+        type="text"
+        placeholder="username"
+        value={input}
+        style={{ width: 200, padding: 20, fontSize: 25, borderRadius: 50 }}
+        onChange={handleChange}
+      />
+      <button style={{ borderRadius: 50, width: 100, height: 50 }} onClick={() => btnClick()}>Submit</button>
+      <ul>{userName.map((name, index) => {
+        return (
+          <li key={index}>{ name}</li>
+        )
+      })}</ul>
     </div>
   );
 }
-
-export default App;
