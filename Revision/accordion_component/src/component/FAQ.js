9f2f21b8265d67ef1a194dccabf2614d0a88d66b
krishna <dhekkrishna1@gmail.com>
2025-05-14 15:14:02 +0530

    Accordion with toggle behaviour

--- Revision/accordion_component/src/component/FAQ.js
+++ Revision/accordion_component/src/component/FAQ.js
@@ -1,22 +1,18 @@
 import React, { useState } from "react";
-import Accordion from "./Accordion";
+import Accordion from "./Accordion"
 
-import "../App.css"
-export default function FAQ({data}) {
-     const [isOpen, setIsOpen] = useState();
+export default function FAQ({ data }) {
+    const [isOpen, setIsOpen] = useState(null);
+    
     const toggle = (index) => {
-        setIsOpen((prev) => (prev === index ? null : index));
-  }
+        setIsOpen((prev)=>(prev===index?null:index))
+    }
     return (
-        <div className="containerFAQ">
-            <h1>FAQ</h1>
-            {data.map((obj, index) => {
-                return (
-                    <Accordion qna={obj} index={index} isOpen={isOpen===index} toggle={toggle} key={index}/>
-                )
-            })}
-            
-        </div>
-        
-    )
+      <div className="containerFAQ">
+        <h1>FAQ</h1>
+        {data.map((item, index) => {
+          return <Accordion isOpen={isOpen===index} toggle={toggle} item={item} index={index} key={index} />;
+        })}
+      </div>
+    );
 }
