21b42b947618106103e896287303687cec4f6a5f
krishna <dhekkrishna1@gmail.com>
2025-05-17 17:21:20 +0530

    Add dot indicators

--- Revision/carousel_component/src/component/Carousel.js
+++ Revision/carousel_component/src/component/Carousel.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 
 export default function Carousel({ data }) {
     const [index, setIndex] = useState(0);
-    const leng = data.length
+    const len = data.length;
     const ref = useRef(null);
+
     const handlePrev = () => {
         setIndex((prev) => {
             if (prev === 0) {
-                return leng - 1;
-            }
-            else{
+                return len - 1;
+            } else {
                 return prev - 1;
             }
         })
@@ -17,32 +17,32 @@ export default function Carousel({ data }) {
 
     const handleNext = () => {
         setIndex((prev) => {
-            if (prev === leng - 1) {
-                return 0;
+            if(prev == len-1){
+            return 0;
             } else {
-                return prev + 1;
+               return prev + 1;
             }
         })
     }
 
-    useEffect(() => {
-        ref.current = setInterval(handleNext, 1000);
-        return ()=>clearInterval(ref.current)
-    },[])
+    
     return (
-      <div className="carousel" onMouseLeave={()=>ref.current = setInterval(handleNext, 1000)} onMouseEnter={()=>clearInterval(ref.current)}>
-        <button onClick={() => handlePrev()}>⬅️</button>
-          <img
-            src={data[index].url}
-            key={data[index].id}
-            style={{ width: '500px', height: '500px', objectFit: 'cover', margin:'0 20px'}}
-          ></img>
-            <button onClick={() => handleNext()}>➡️</button>
+        <div className="carousal">
+            <div className="leftBtn" onClick={()=>handlePrev()}>{"<"}</div>
+            <img src={data[index].url}></img>
+            <div className="rightBtn" onClick={() => handleNext()}>{">"}</div>
             <div className="dotContianer">
-                {data.map((_, index)=>{
-                    return <span key={index}>&#x2022;</span>;
+                {data.map((_, dotIndex) => {
+                    return (
+                        <span
+                            onClick={() => setIndex(dotIndex)}
+                            key={dotIndex}
+                            className={dotIndex===index?"dot":""}
+                        >&#x2022;</span>
+                    )
                 })}
             </div>
-      </div>
-    );
+            
+        </div>
+    )
 }
