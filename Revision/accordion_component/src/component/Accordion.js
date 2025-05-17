5cce7565a8c2f3604977876869af9f060b1c3610
krishna <dhekkrishna1@gmail.com>
2025-05-17 17:00:40 +0530

    Practice Question

--- Revision/accordion_component/src/component/Accordion.js
+++ Revision/accordion_component/src/component/Accordion.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-export default function Accordion({item, isOpen, toggle, index}) {
+export default function Accordion({ item, index, isOpen,toggle }) {
+   
     return (
         <div className="accordion">
             <div className="question" onClick={()=>toggle(index)}>
@@ -8,7 +9,7 @@ export default function Accordion({item, isOpen, toggle, index}) {
                     <span>{isOpen?"-":"+" }</span>
                 </h1>
             </div>
-            {isOpen ? <p className="answer">{item.answer }</p>:""}
+            {isOpen ? <p className="answer">{ item.answer}</p>:""}
         </div>
     )
 }
--- Revision/accordion_component/src/component/FAQ.js
+++ Revision/accordion_component/src/component/FAQ.js
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import Accordion from "./Accordion"
+import Accordion from "./Accordion";
 
 export default function FAQ({ data }) {
-    const [isOpen, setIsOpen] = useState(null);
+    const [isOpen, setIsOpen] = useState(0);
+     const toggle = (index) => {
+       setIsOpen((prev)=>(prev===index)?null:index);
+     };
 
-    const toggle = (index) => {
-        setIsOpen((prev) => (prev === index ? null : index));
-    }
     return (
-        <div className="containerFAQ">
-            <h1>FAQ</h1>
+        <div>
+            <h1>FAQs</h1>
             {data.map((item, index) => {
-               return <Accordion item={item} isOpen={isOpen===index} toggle={toggle} index={index} key={index} />;
-           })}
+                return <Accordion item={item} isOpen={ isOpen===index} key={index} index={index} toggle={toggle} />
+            })}
         </div>
     )
 }
