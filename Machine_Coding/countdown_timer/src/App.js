b99d0e595fb43e397fd214f582389db85c114857
krishna <dhekkrishna1@gmail.com>
2025-05-22 00:40:33 +0530

    Countdown timer in react

--- Machine_Coding/countdown_timer/src/App.js
+++ Machine_Coding/countdown_timer/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import CountDownTimer from "./component/CountDownTimer";
+import "./App.css"
 
 export default function App() {
   return (
     <div>
-      <h1 style={{textAlign:'center'}}>CountDown Timer</h1>
+      <h1 style={{ textAlign: 'center' }}>CountDown Timer</h1>
+      <CountDownTimer />
     </div>
   )
 }
