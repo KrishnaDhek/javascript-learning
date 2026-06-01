9a33fec16fcc2dbe4d32586e160ad97d66ee34ee
krishna <dhekkrishna1@gmail.com>
2026-06-01 22:28:08 +0530

    Add solution for Container With Most Water in O(n) time, and O(1) space

---
+++ Problem_Solving/ContainerWithMostWater.js
@@ -0,0 +1,21 @@
+function containerWithMostWater(height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxWater = 0;
+
+  while (left < right) {
+    const area = Math.min(height[left], height[right]) * (right - left);
+
+    maxWater = Math.max(maxWater, area);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxWater;
+}
+
+console.log(containerWithMostWater([1, 8, 6, 2, 5, 4, 8, 3, 7])); // 49
